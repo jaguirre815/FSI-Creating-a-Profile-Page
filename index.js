1584a983246ddca39dcc22a6562090a7bc3d47a0
Joel Aguirre <Joels@Joshuas-MacBook-Pro.local>
2022-02-02 20:49:04 -0600

    a little info about a dog

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 let content = document.querySelector('.js-generated.content')
+
+// create header
+let header = document.createElement('h1')
+// add class dog-name
+header.setAttribute('class', 'dog-name')
+// append the header to the content
+header.append('Rizzo')
+content.append(header)
+
+
+
+
